d724dbd70aaa0769c12268485463eed933a01b2b
Andres Navarro S <andresm.navarro@gmail.com>
2024-06-02 11:42:52 -0400

    Add format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,16 +10,30 @@ return {
 	},
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
 				require("none-ls.diagnostics.eslint_d"),
-        -- null_ls.builtins.diagnostics.eslint_d,
+				-- null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.formatting.prettier,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
